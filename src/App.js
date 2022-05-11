0def009d28215d59e2ae4ac2f9ba5b5bdc9be072
Benstewart17 <100318606+Benstewart17@users.noreply.github.com>
2022-05-11 17:16:32 -0400

    upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-       <h1>Hello World</h1>
+       <h1>Weather App</h1>
+      <Weather defaultCity ="Columbus" />
       </header>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://polite-lolly-42a4da.netlify.app/" target="_blank">
+          Ben Stewart
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Benstewart17/weather-react-three" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+      </div>
     </div>
+
   );
 }
 
