6dff29c76958438f3f56f7c870e6afb421c9bfb6
Benstewart17 <100318606+Benstewart17@users.noreply.github.com>
2022-05-11 17:47:47 -0400

    trial and error 1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from "react";
 import Weather from "./Weather";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
       <header className="App-header">
        <h1>Weather App</h1>
-      <Weather defaultCity ="Columbus" />
       </header>
+      <Weather defaultCity ="Columbus" />
       <footer>
         This project was coded by{" "}
         <a href="https://polite-lolly-42a4da.netlify.app/" target="_blank" rel="noopener noreferrer">
           Ben Stewart
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/Benstewart17/weather-react-three" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/Benstewart17/project-react-weather-app" target="_blank" rel="noopener noreferrer">
           open-sourced on GitHub
-        </a>
+        </a> and{" "}
+        <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
       </footer>
       </div>
     </div>
@@ -26,4 +33,4 @@ function App() {
   );
 }
 
-export default App;
+
