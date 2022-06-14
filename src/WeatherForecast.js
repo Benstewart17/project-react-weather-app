348493d986047ce1e168f7a461c0ba4409d698e6
Benstewart17 <100318606+Benstewart17@users.noreply.github.com>
2022-06-14 19:33:49 -0400

    changed to imperial

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,7 +20,7 @@ export default function WeatherForecast(props) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
     axios.get(apiUrl).then(handleResponse);
   }
