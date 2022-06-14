efb6c9d340fdef16cee9076c60a2eb5cb07862b3
Benstewart17 <100318606+Benstewart17@users.noreply.github.com>
2022-06-14 19:34:57 -0400

    Changed C to F

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -4,7 +4,7 @@ export default function WeatherTemperature(props) {
   return (
     <div className="WeatherTemperature">
       <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
+      <span className="unit">°F</span>
     </div>
   );
 }
